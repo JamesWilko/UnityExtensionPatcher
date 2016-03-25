20783b73c86068ef047969b04add351d2f833761
James Wilkinson <jw@jameswilko.com>
2016-03-25 14:02:36 +1100

    MRUController uses WPF instead of Winforms

--- UnityPatcher/Utils/MRUController.cs
+++ UnityPatcher/Utils/MRUController.cs
@@ -4,19 +4,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.Windows.Controls;
 
-namespace UnityExtensionPatcher
+namespace UnityPatcher.Utils
 {
+
 	public class MRUController
 	{
 		public string ApplicationName { get; private set; }
 		public string SubKeyName { get; private set; }
 		public int MaximumEntries { get; private set; }
-		public Action<object, EventArgs> OnRecentFileClicked { get; private set; }
-		public ToolStripMenuItem ParentMenuItem { get; private set; }
+		public Action<string> OnRecentFileClicked { get; private set; }
+		public MenuItem ParentMenuItem { get; private set; }
 
-		public MRUController(string applicationName, int maxEntries, ToolStripMenuItem parentMenu, Action<object, EventArgs> onRecentFileClick)
+		public MRUController(string applicationName, int maxEntries, MenuItem parentMenu, Action<string> onRecentFileClick)
 		{
 			this.ApplicationName = applicationName;
 			this.MaximumEntries = maxEntries;
@@ -70,7 +71,7 @@ namespace UnityExtensionPatcher
 			{
 				Console.WriteLine($"{exception.Message}{Environment.NewLine}{exception.StackTrace}");
 				registryKey?.Dispose();
-                return;
+				return;
 			}
 
 			// Update the MRU list on the form
@@ -95,7 +96,7 @@ namespace UnityExtensionPatcher
 				foreach (var file in recentFiles)
 				{
 					string registryName = (string)registryKey.GetValue(file, null);
-                    if (registryName != null && registryName == fileName)
+					if (registryName != null && registryName == fileName)
 					{
 						registryKey.DeleteValue(file, true);
 					}
@@ -129,25 +130,25 @@ namespace UnityExtensionPatcher
 				}
 
 				// Remove the old list
-				ParentMenuItem.DropDownItems.Clear();
+				ParentMenuItem.Items.Clear();
 
 				// Find the most recent files
 				var recentFiles = registryKey.GetValueNames();
-				var recentFileList = recentFiles.Select(entry => (string) registryKey.GetValue(entry));
+				var recentFileList = recentFiles.Select(entry => (string)registryKey.GetValue(entry));
 				recentFileList = recentFileList.Where(file => file != null);
 
 				// Populate the most recent files list
-                foreach (var file in recentFileList)
+				foreach (var file in recentFileList)
 				{
-                    ToolStripItem toolStripItem = ParentMenuItem.DropDownItems.Add(file);
-					toolStripItem.Click += new EventHandler(OnRecentFileClicked);
+					var recentFileButton = new MenuItem();
+					recentFileButton.Header = file;
+					recentFileButton.Click += RecentFileButton_Click;
+					ParentMenuItem.Items.Add(recentFileButton);
 				}
 
 				// Add a clear list button 
-				ParentMenuItem.DropDownItems.Add("-");
-				ToolStripItem clearItemsStripItem = ParentMenuItem.DropDownItems.Add("Clear Recent Files");
-				clearItemsStripItem.Click += new EventHandler(ClearList);
-			}
+				AddClearRecentButton();
+            }
 			catch (Exception exception)
 			{
 				Console.WriteLine($"{exception.Message}{Environment.NewLine}{exception.StackTrace}");
@@ -155,6 +156,27 @@ namespace UnityExtensionPatcher
 				return;
 			}
 		}
+
+		protected void AddClearRecentButton()
+		{
+			ParentMenuItem.Items.Add(new Separator());
+
+			var clearRecentButton = new MenuItem();
+			clearRecentButton.Header = "Clear Recent Files";
+			clearRecentButton.Click += ClearRecentButton_Click;
+			ParentMenuItem.Items.Add(clearRecentButton);
+		}
+
+		private void RecentFileButton_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			if(OnRecentFileClicked != null)
+				OnRecentFileClicked((sender as MenuItem).Header.ToString());
+        }
+
+		private void ClearRecentButton_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			ClearList(sender, e);
+        }
 
 		private void ClearList(object sender, EventArgs e)
 		{
@@ -173,7 +195,9 @@ namespace UnityExtensionPatcher
 			}
 
 			// Clear the list in the editor
-			ParentMenuItem.DropDownItems.Clear();
+			ParentMenuItem.Items.Clear();
+			AddClearRecentButton();
 		}
 	}
+
 }
